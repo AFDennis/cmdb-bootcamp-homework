1e9f23e50a91e34e363e6a40fb326867bab64759
Allison Dennis <allisondennis717@gmail.com>
2014-09-02 20:23:21 -0400

    fixed script

--- day1-homework_round2.sh
+++ day1-homework_round2.sh
@@ -14,6 +14,6 @@ ANNOTATION=dmel-all-r5.57.gff
 CORES=4
 for i in {893..916}
   do echo fastqc $FASTQ_DIR/$FASTQ_FILE$i\.fastq.gz -o $OUTPUT_DIR
-  echo tophat -p 4 -G $OUTPUT_DIR/$ANNOTATION -o $OUTPUT_DIR\.tophat.$FASTQ_FILE$i --no-novel-juncs --segment-length 20 $GENOME_DIR/$GENOME_INDEX $FASTQ_DIR$FASTQ_FILE$i\.fastq.gz
+  echo tophat -p 4 -G $OUTPUT_DIR/$ANNOTATION -o $OUTPUT_DIR\.tophat.$FASTQ_FILE$i --no-novel-juncs --segment-length 20 $GENOME_DIR/$GENOME_INDEX $FASTQ_DIR/$FASTQ_FILE$i\.fastq.gz
   echo cufflinks -p 4 -G $OUTPUT_DIR/$ANNOTATION -o $OUTPUT_DIR\.cufflinks.$FASTQ_FILE$i $OUTPUT_DIR\.tophat.$FASTQ_FILE$i/accepted_hits.bam
 done
